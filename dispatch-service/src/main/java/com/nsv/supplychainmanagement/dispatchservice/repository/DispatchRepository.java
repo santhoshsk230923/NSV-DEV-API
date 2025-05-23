4888f384ff374f638f5418e4476b9f6a91c06503
santhoshsk230923 <149874991+santhoshsk230923@users.noreply.github.com>
2025-05-23 22:48:05 +0530

    refactor the properties files based on profile

--- dispatch-service/src/main/java/com/nsv/supplychainmanagement/dispatchservice/repository/DispatchRepository.java
+++ dispatch-service/src/main/java/com/nsv/supplychainmanagement/dispatchservice/repository/DispatchRepository.java
@@ -2,8 +2,6 @@ package com.nsv.supplychainmanagement.dispatchservice.repository;
 
 import com.nsv.supplychainmanagement.dispatchservice.model.Dispatch;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface DispatchRepository extends JpaRepository<Dispatch, Integer> {
 }
